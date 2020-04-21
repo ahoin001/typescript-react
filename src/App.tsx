71e5b3227dc7551f7adc0166276d6dbdd66078f9
Alexander Hoinville <ahoin001@gmail.com>
2020-04-20 23:57:01 -0400

    Use TS to type proof props on component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TodoList from './components/TodoList';
 
-function App() {
+
+// ? React Type provided by react
+const App: React.FC = () => {
+
+  const todos = [{id:'t1', text:'Learn TypeScript With react'}]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {/* Component That Adds Tasks */}
+
+      <TodoList items={todos}/>
+    
     </div>
   );
+
 }
 
 export default App;
